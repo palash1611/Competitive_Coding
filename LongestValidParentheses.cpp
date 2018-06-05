9748c96bd43cf004e1b450b3fbde0a780e450398
palash1611 <palashchaturvedi1611@gmail.com>
2018-06-05 09:31:03 +0530

    commit

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -1,4 +1,46 @@
 //https://practice.geeksforgeeks.org/problems/longest-valid-parentheses/0
+
+//Alt SOl (better complexity)
+/*
+
+#include <bits/stdc++.h>
+#define li long long
+#define MOD 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    li t;
+    cin>>t;
+    while(t--){
+       string s;
+       cin>>s;
+       stack<pair<char,li>>q;
+       li maxi=0;
+       q.push({'f',-1});
+       for(li i=0;i<s.size();i++){
+           if(s[i]=='('){
+                q.push({'(',i});
+              }
+            else if(s[i]==')'){
+                if(!q.empty()&&q.top().first=='('){
+                    q.pop();
+                    maxi=max(maxi,i-q.top().second);
+                }
+                else{
+                    q.push({')',i});
+                }
+            }
+       }
+       cout<<maxi<<endl;
+    }
+}
+*/
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
--- test.cpp
+++ test.cpp
@@ -1,37 +1,121 @@
-#include<bits/stdc++.h>
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        //int t=0;
+        int flag=0;
+        int ans=0;
+        int clap=0;
+        int var=0;
+        int k=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            {
+                k++;
+                if(var)
+                k=1;
+                if(flag)
+                {
+                flag=0;
+                clap=0;
+                }
+                var=1;
+            }
+            else
+            {
+                if(k>=1)
+                {
+                  //var=2+var;
+                  k--;
+                  flag=1;
+                  clap=1;
+                }
+                if(clap)
+                ans=ans+2;
+                var=0;
+            }
+            cout<<s[i]<<" flag "<<flag<<" ans "<<ans<<" clap "<<clap<<" k "<<k<<" var "<<var<<endl;
+
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+/*
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-  string arr="(()()(";
-  stack<char> s;
-  int l,r;
-  cin>>l>>r;
-  cout<<arr[0]<<endl;
-  cout<<arr[1]<<endl;
-  cout<<arr[2]<<endl;
-  cout<<arr[3]<<endl;
-  cout<<arr[4]<<endl;
-  cout<<arr[5]<<endl;
-  for(int i = l; i <=r ; i++)
+  int t;
+  cin>>t;
+  while(t--)
   {
-
-    if(arr[i]=='(')
-    s.push(arr[i]);
-
-    if(arr[i]==')' && !s.empty())
-    s.pop();
-    cout<<arr[i]<<" ";
+      string s;
+      cin>>s;
+      int ans=0;
+      int k=0;
+      for(int i=0;i<s.length();i++)
+      {
+          if(s[i]=='(')
+          {
+              k++;
+          }
+          else
+          {
+              if(k>=1)
+              {
+                k--;
+              ans=ans+2;
+              }
+          }
+      }
+      cout<<ans<<endl;
   }
-  cout<<endl;
-  if(s.empty())
-  {
-    cout<<l<<" "<<r<<" valid"<<endl;
-
-  }
-  else
-  {cout<<l<<" "<<r<<" not valid hui hui"<<endl;
-  }
-
   return 0;
-}
+}*/
+
+
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char str[10000];
+int i, cur, mx=0;
+int main(){
+	int t;
+	cin>>t;
+	stack <int> s;
+	while(t--){
+		cin>>str;
+		for (i=0; i<strlen(str); i++){
+			if (str[i] == '('){
+				s.push(cur);
+				cur=0;
+			}
+			if (str[i] == ')' && s.size()>0){
+				cur = s.top() + 2;
+				s.pop();
+				mx = max(mx, cur);
+			}
+			if (str[i] == ')' && s.size()==0){
+				cur = 0;
+			}
+		}
+		cout<<mx<<endl;
+	}
+	return 0;
+}*/
